ea87b59f55520f5a02af960ad7d8ba0934bcfb98
subhamc88 <subham020304@gmail.com>
2024-04-11 22:02:40 +0530

    clear button is working

--- script.js
+++ script.js
@@ -1,51 +1,56 @@
-const canvas = document.getElementById('canvas');
+const canvas = document.getElementById('canvas')
+const clearCanvas = document.getElementById('clear-btn')
 
-const ctx = canvas.getContext('2d');
+const ctx = canvas.getContext('2d')
+let x, y
+let size = 10
+let color = 'black'
+let isPressed = false
 
-let x, y;
-let size = 10;
-let color = 'black';
-let isPressed = false;
-
+// Canvas Event Listner
 canvas.addEventListener('pointerdown', (e) => {
-  const rect = canvas.getBoundingClientRect();
-  x = e.clientX - rect.left;
-  y = e.clientY - rect.top;
-  isPressed = true;
-  point(x, y);
-});
-
+  x = e.offsetX
+  y = e.offsetY 
+  isPressed = true
+  point(x, y)
+})
 canvas.addEventListener('mousemove', (e) => {
   if (isPressed) {
-    const rect = canvas.getBoundingClientRect();
-    const x2 = e.clientX - rect.left;
-    const y2 = e.clientY - rect.top;
-    line(x, y, x2, y2);
-    x = x2;
-    y = y2;
+    const x2 = e.offsetX
+    const y2 = e.offsetY
+    line(x, y, x2, y2)
+    x = x2
+    y = y2
   }
-});
-
+})
 canvas.addEventListener('pointerup', (e) => {
-  isPressed = false;
-  x = undefined;
-  y = undefined;
-});
+  isPressed = false
+  x = undefined
+  y = undefined
+})
+
+// Button Event Listner
+clearCanvas.addEventListener('click',()=>{
+  ctx.clearRect(0,0,canvas.width,canvas.height)
+  isPressed = false
+  x = undefined
+  y = undefined
+})
 
 const point = (x, y) => {
-  ctx.beginPath();
-  ctx.arc(x, y, size, 0, Math.PI * 2);
-  ctx.fillStyle = color;
-  ctx.fill();
-};
+  ctx.beginPath()
+  ctx.arc(x, y, size, 0, Math.PI * 2)
+  ctx.fillStyle = color
+  ctx.fill()
+}
 
 const line = (x1, y1, x2, y2) => {
-  ctx.beginPath();
-  ctx.moveTo(x1, y1);
-  ctx.lineTo(x2, y2);
-  ctx.strokeStyle = color;
-  ctx.lineWidth = size * 2;
-  ctx.lineCap = 'round';
-  ctx.lineJoin = 'round';
-  ctx.stroke();
-};
+  ctx.beginPath()
+  ctx.moveTo(x1, y1)
+  ctx.lineTo(x2, y2)
+  ctx.strokeStyle = color
+  ctx.lineWidth = size * 2
+  ctx.lineCap = 'round'
+  ctx.lineJoin = 'round'
+  ctx.stroke()
+}
